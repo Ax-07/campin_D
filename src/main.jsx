181420070ae4d0bb73c99318d02e5f2bfe24782c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-29 15:59:30 +0100

    fix chemin d'acces

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "/src/App.jsx";
+import { App } from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 import "/src/assets/scss/index.css";
