cd8b2f6241efcf69bdcd1ffc0e9dc64645ccf60d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-04 12:23:45 +0100

    ajout chaletApi

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 import { tarifsApi } from '../services/api/tarifsApi';
+import { chaletApi } from '../services/api/chaletApi';
 
 export const store = configureStore({
     reducer: {
         [tarifsApi.reducerPath]: tarifsApi.reducer,
+        [chaletApi.reducerPath]: chaletApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(tarifsApi.middleware),
+        getDefaultMiddleware().concat(tarifsApi.middleware, chaletApi.middleware),
     });
 
 setupListeners(store.dispatch);
