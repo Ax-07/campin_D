07ae7527383eb1c911952a4c749ffc164dfca829
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-04 12:11:22 +0100

    ajout api chalet

--- src/services/api/chaletApi.js
+++ src/services/api/chaletApi.js
@@ -1,3 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const chaletApi = createApi({
+    reducerPath: 'chaletApi',
+    baseQuery: fetchBaseQuery({ baseUrl: '/camping_D' }),
+    endpoints: (builder) => ({
+        getChalets: builder.query({
+            query: () => `/db/descriptifChalet.json`,
+        }),
+    })
+})
+
+export const { useGetChaletsQuery } = chaletApi;
+
+
 export const getDataChalets = async () => {
     try {
         const response = await fetch('./db/descriptifChalet.json');
