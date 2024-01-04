4b291e6c3e07ae4071f3ce92cda48f05aedd96df
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-04 12:15:24 +0100

    ajout contact form

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import { HeroBanner } from "../components/HeroBanner.jsx";
 import { useState } from "react";
 import { getAllData } from "../services/api/logementApi.js";
 import { useEffect } from "react";
+import { ContactForm } from "../components/ContactForm.jsx";
 // import { DropDown } from "../components/DropDown.jsx";
 
 export const About = () => {
@@ -30,6 +31,7 @@ export const About = () => {
             </DropDown>
           ))}
       </div> */}
+      <ContactForm />
       <div className="google-map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11273.355575371896!2d4.385802174333203!3d45.05863599273192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f582c0cead31b1%3A0xa2b406d6fd56610f!2sCamping%20du%20Lac%20de%20Devesset!5e0!3m2!1sfr!2sfr!4v1703929574474!5m2!1sfr!2sfr"
