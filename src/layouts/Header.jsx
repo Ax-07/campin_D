8e96b63c6f23243787de5a6768095b9259a71592
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-07 15:25:18 +0100

    modif btn burger

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,19 +4,13 @@ import logo from "/images/coloredLogoDevesset3.png";
 import { BtnBurger } from "../components/BtnBurger";
 
 export const Header = () => {
-
   return (
     <div className="header">
-            <NavLink className="header__logo navBar__link" to="/">
-        <img className="header__logo-img" src={logo} alt="Logo" />
-        </NavLink>
-      <NavBar className="header__nav" />
-      <BtnBurger>
       <NavLink className="header__logo navBar__link" to="/">
         <img className="header__logo-img" src={logo} alt="Logo" />
-        </NavLink>
-      <NavBar className="header__nav" />
-      </BtnBurger>
+      </NavLink>
+      <BtnBurger/>
+        <NavBar className="header__nav" />
     </div>
   );
 };
