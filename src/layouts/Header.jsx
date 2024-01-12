53758e208c08083073a2376f1320e16e81d99188
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-12 18:46:56 +0100

    ajout condition rendu composant

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,16 +1,27 @@
 import { NavLink } from "react-router-dom";
 import { NavBar } from "/src/components/NavBar.jsx";
-import logo from "/images/coloredLogoDevesset3.png";
+import logo from "/images/logo2.png";
 import { BtnBurger } from "../components/BtnBurger";
+import { NavLeft } from "./NavLeft";
+import { useWindowSize } from "../utils/hooks/useWindowSize";
+import { NavLeftProvider } from "../contexts/navbarContext";
 
 export const Header = () => {
+  const { windowWidth } = useWindowSize();
+
   return (
-    <div className="header">
+    <header className="header">
       <NavLink className="header__logo navBar__link" to="/">
         <img className="header__logo-img" src={logo} alt="Logo" />
       </NavLink>
-      <BtnBurger/>
-        <NavBar className="header__nav" />
-    </div>
+      <NavLeftProvider>
+      {windowWidth < 1024 ? (
+        <>
+          <BtnBurger windowWidth={windowWidth}/>
+          <NavLeft className="header__navLeft"/>
+        </>
+      ) : (<NavBar className="header__nav" />)}
+      </NavLeftProvider>
+    </header>
   );
 };
