3ce61f0ffbb7b15974d51e2c96a0b687a1c5f0e5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-12 18:43:47 +0100

    delete code inutile

--- src/pages/Tarifs.jsx
+++ src/pages/Tarifs.jsx
@@ -1,11 +1,10 @@
-import { HeroBanner } from "../components/HeroBanner.jsx";
 import { TarifsList } from "../layouts/TarifsList.jsx";
 import { DropDown } from "../components/DropDown.jsx";
 import { useGetAllDataQuery } from "../services/api/tarifsApi.js";
 
 export const Tarifs = () => {
     const heroImg = './images/vueDuLac/20211011_194517.jpg';
-    const { data: tarifs = [], refetch: refetchTarifs } = useGetAllDataQuery();
+    const { data: tarifs = [] } = useGetAllDataQuery();
 
     return (
     <section className="section section__contents" style={{backgroundImage: `url(${heroImg})`}}>
