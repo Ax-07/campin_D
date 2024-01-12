5d337af092ee12466634e5b1bd14e54445df2512
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-12 18:42:00 +0100

    ajout hooks scroll et windowsize

---
+++ src/utils/hooks/useScrollToTarget.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export const useScrollToTarget = () => {
+    useEffect(() => {
+        const links = document.querySelectorAll('.topNav__link');
+        links.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                const targetId = link.getAttribute('data-target'); // exemple: <a href="#about" data-target="#about">About</a>
+                const targetSection = document.querySelector(targetId);
+                links.forEach(link => {
+                    if (link.classList.contains('active')) {
+                        link.classList.remove('active');
+                    }
+                });          
+                link.classList.add('active');
+                
+                if (targetSection) {
+                    targetSection.scrollIntoView({
+                        behavior: 'smooth',
+                        block:"start"
+                    });
+                }
+            });
+        });
+    },[])};
