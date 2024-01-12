84466753ebefff34949eb8c8a9219598fe254114
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-12 18:43:22 +0100

    refactoring

--- src/utils/functions/scrollEvents.js
+++ src/utils/functions/scrollEvents.js
@@ -6,25 +6,10 @@ export const displayElementOnScroll = (el) => {
         let logoElement = document.querySelector(el.logo);
 
         if (headerElement && logoElement) {
-            if (currentScrollPosition < 10) {
-                // Faire disparaître la barre de navigation
-                headerElement.style.top = "-168px";
-                // place le logo au centre
-                logoElement.style.position = "absolute";
-                logoElement.style.left = "calc(50%)";
-                logoElement.style.top = "168px";
-                logoElement.style.width = "200px";
-                logoElement.style.height = "200px";
-
+            if (currentScrollPosition < 20) {
+                headerElement.classList.remove('header--display');
             } else {
-                // Faire apparaître la barre de navigation
-                headerElement.style.top = "0px";
-                // place le logo à gauche la barre de navigation
-                logoElement.style.position = "relative";
-                logoElement.style.left = "50px";
-                logoElement.style.top = "0px";
-                logoElement.style.width = "75px";
-                logoElement.style.height = "75px";
+                headerElement.classList.add('header--display');
             }
         }
     }
