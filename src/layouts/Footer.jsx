4f5733b7865eef1178f2c9e8825de4800f8015d9
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-12 18:54:06 +0100

    modif miner

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
 import logo from "/images/coloredLogoDevesset.png";
-
+import { DropLink } from "../components/DropLink";
 
 export const Footer = () => {
-    return (
-        <div className="footer">
+  return (
+    <footer className="footer">
       <NavLink className="navBar__link" to="/">
         <img className="footer__logo logo" src={logo} alt="Logo" />
-        </NavLink>
-            <div className="footer__copyright">Camping du lac de Devesset</div>
-        </div>
-    );
+      </NavLink>
+      <DropLink title="Mentions Legales" to="/">
+        <NavLink className="navBar__link" to="/">Contact</NavLink>
+        <NavLink className="navBar__link" to="/">Accés</NavLink>
+      </DropLink>
+      <div className="footer__copyright">Camping du lac de Devesset</div>
+    </footer>
+  );
 };
